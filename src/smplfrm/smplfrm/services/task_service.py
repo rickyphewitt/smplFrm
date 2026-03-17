4c5cf85ac988a9cb7ce6347da9e98fc1c834b3f6
Ricky <rickyphewitt@pm.me>
2026-03-16 18:14:44 -0700

    Enforce one pending/running task for a given Task Type (#115)
    fixes #110 Ensure only a single task type can be started. Includes an error message to the user via a toast.

--- src/smplfrm/smplfrm/services/task_service.py
+++ src/smplfrm/smplfrm/services/task_service.py
@@ -4,6 +4,7 @@ from typing import Any, Dict
 from django.db.models import QuerySet
 
 from smplfrm.models import Task
+from smplfrm.models.task import Status, TaskType
 
 from .base_service import BaseService
 
@@ -21,7 +22,19 @@ class TaskService(BaseService):
 
         Returns:
             Created Task instance
+
+        Raises:
+            IntegrityError: If a task of the same type is already pending or running
         """
+        from django.db import IntegrityError
+
+        task_type = data.get("task_type")
+        if Task.objects.filter(
+            task_type=task_type, status__in=[Status.PENDING, Status.RUNNING]
+        ).exists():
+            raise IntegrityError(
+                f"A {TaskType(task_type).label} task is already pending or running."
+            )
         return Task.objects.create(**data)
 
     def read(self, ext_id: str, deleted: bool = False) -> Task:
