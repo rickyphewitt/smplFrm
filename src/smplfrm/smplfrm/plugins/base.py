b4ee1928c3c27cd8908fe3580393d9188fdf9571
Ricky Hewitt <rickyphewitt@pm.me>
2026-03-29 06:05:11 -0700

    Removing properties from plugin base

--- src/smplfrm/smplfrm/plugins/base.py
+++ src/smplfrm/smplfrm/plugins/base.py
@@ -4,24 +4,21 @@ class BasePlugin:
     def __init__(self, name: str, description: str, settings: dict = None):
         self.name = name
         self.description = description
-        self._default_settings = settings if settings is not None else {}
+        self.settings = settings if settings is not None else {}
 
-    @property
     def enabled(self) -> bool:
         """Check if this plugin is enabled in the active config."""
         from smplfrm.services.config_service import ConfigService
 
         return self.name in ConfigService().load_config().plugins
 
-    @property
-    def plugin_settings(self) -> dict:
+    def get_plugin_settings(self) -> dict:
         """Load this plugin's settings from the database."""
         from smplfrm.services.plugin_service import PluginService
 
         return PluginService().read_by_name(self.name).settings
 
-    @plugin_settings.setter
-    def plugin_settings(self, value: dict):
+    def update_plugin_settings(self, value: dict):
         """Update this plugin's settings in the database."""
         from smplfrm.services.plugin_service import PluginService
 
